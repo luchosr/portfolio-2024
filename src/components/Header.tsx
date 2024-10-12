e217af72b0cee6531427acbdbfb751f8994a6b17
Luciano Ramello <luchosr@gmail.com>
2024-10-12 22:50:32 +0200

    style: improves responsiveness

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,10 +2,12 @@ import profilePic from '../images/profilePic.webp';
 
 export default function Header() {
   return (
-    <header className="flex flex-row items-center  mx-auto mt-24">
-      <div className="mt-2 flex-1 md:mt-0 w-1/2">
-        <h1 className="text-3xl no-underline">Hey, I&#39;m Luciano.</h1>
-        <p className="mt-3  text-muted-foreground w-3/4">
+    <header className="flex  flex-col md:flex-row items-center  mx-auto mt-24">
+      <div className="mt-2 flex-1 md:mt-0 md:w-2/3 ">
+        <h1 className="text-3xl no-underline text-center md:text-left">
+          Hey, I&#39;m Luciano.
+        </h1>
+        <p className="mt-3 text-center mb-6 md:text-left  w-11/12 ">
           I&#39;m a <span className="font-bold">React.js | Next.js</span>{' '}
           frontend developer based in Madrid, Spain. I&#39;m passionate about
           learning new technologies as <span className="font-bold">Rust</span>,
