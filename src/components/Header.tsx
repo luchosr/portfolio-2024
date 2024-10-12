58c1fffdfdc651d4de3bc827cea946ae9edb58dd
Luciano Ramello <luchosr@gmail.com>
2024-10-12 21:08:14 +0200

    feat: improves header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,29 +1,32 @@
-import React from 'react';
+import profilePic from '../images/profilePic.webp';
 
 export default function Header() {
   return (
-    <header className="fixed inset-x-0 top-0 z-50 bg-background/75 py-6 backdrop-blur-sm">
-      <nav className="container mx-auto flex max-w-3xl items-center justify-between">
-        <div>
-          <a href="/" className="font-serif text-2xl font-bold">
-            LR
-          </a>
-        </div>
-
-        <ul className="flex items-center gap-6 text-sm font-light text-muted-foreground sm:gap-10">
-          <li className="transition-colors hover:text-foreground">
-            <a href="/posts">Posts</a>
-          </li>
-          <li className="transition-colors hover:text-foreground">
-            <a href="/projects">Projects</a>
-          </li>
-          <li className="transition-colors hover:text-foreground">
-            <a href="/contact">Contact</a>
-          </li>
-        </ul>
-
-        <div>{/* <ThemeToggle /> */}</div>
-      </nav>
+    <header className="flex flex-row items-center  mx-auto mt-24">
+      <div className="mt-2 flex-1 md:mt-0 w-1/2">
+        <h1 className="text-3xl no-underline">Hey, I&#39;m Luciano.</h1>
+        <p className="mt-3  text-muted-foreground w-3/4">
+          I&#39;m a <span className="font-bold">React.js | Next.js</span>{' '}
+          frontend developer based in Madrid, Spain. I&#39;m passionate about
+          learning new technologies as <span className="font-bold">Rust</span>,
+          and <span className="font-bold">TypeScript</span> and sharing
+          knowledge with others.
+        </p>
+      </div>
+      <div className="relative">
+        <a
+          href="https://www.linkedin.com/in/luciano-ramello-b45411143/"
+          target="_blank"
+        >
+          <img
+            className="flex-1 rounded-lg grayscale hover:grayscale-0 transition cursor-pointer"
+            src={profilePic}
+            alt="Luciano Ramello"
+            width={150}
+            height={175}
+          />
+        </a>
+      </div>
     </header>
   );
 }
