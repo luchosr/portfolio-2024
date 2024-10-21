435d723412ca88b22bd0b93e6366e78ee6a226be
Luciano Ramello <luchosr@gmail.com>
2024-10-21 20:47:19 +0200

    feat: adds dark mode button component

---
+++ src/components/DarkModeButton.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { IoMoon, IoSunny } from "react-icons/io5";
+
+type DarkModeButtonProps = {
+  dark: boolean;
+  darkModeHandler: () => void;
+};
+
+export default function DarkModeButton({
+  dark,
+  darkModeHandler,
+}: DarkModeButtonProps) {
+  return (
+    <div className="bg-orange-100 dark:bg-gray-800">
+      <button onClick={() => darkModeHandler()}>
+        {dark && <IoMoon className="text-2xl text-gray-700" />}
+        {!dark && <IoSunny className="text-2xl text-orange-200" />}
+      </button>
+    </div>
+  );
+}
