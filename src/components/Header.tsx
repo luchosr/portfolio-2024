5d9c27ae177b1c4a60386cbdf7f2c433f1d9e9e9
Luciano Ramello <luchosr@gmail.com>
2024-10-21 20:47:41 +0200

    style: upgrades header classes

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,14 +1,14 @@
-import profilePic from '../images/profilePic.webp';
+import profilePic from "../images/profilePic.webp";
 
 export default function Header() {
   return (
-    <header className="flex  flex-col md:flex-row items-center  mx-auto mt-24">
-      <div className="mt-2 flex-1 md:mt-0 md:w-2/3 ">
-        <h1 className="text-3xl no-underline text-center md:text-left">
+    <header className="mx-auto mt-24 flex max-w-2xl flex-col items-center md:flex-row">
+      <div className="mt-2 flex-1 md:mt-0 md:w-2/3">
+        <h1 className="text-center text-3xl no-underline md:text-left">
           Hey, I&#39;m Luciano.
         </h1>
-        <p className="mt-3 text-center mb-6 md:text-left  w-11/12 ">
-          I&#39;m a <span className="font-bold">React.js | Next.js</span>{' '}
+        <p className="mb-6 mt-3 w-11/12 text-center md:text-left">
+          I&#39;m a <span className="font-bold">React.js | Next.js</span>{" "}
           frontend developer based in Madrid, Spain. I&#39;m passionate about
           learning new technologies as <span className="font-bold">Rust</span>,
           and <span className="font-bold">TypeScript</span> and sharing
@@ -21,7 +21,7 @@ export default function Header() {
           target="_blank"
         >
           <img
-            className="flex-1 rounded-lg grayscale hover:grayscale-0 transition cursor-pointer"
+            className="flex-1 cursor-pointer rounded-lg bg-white/30 grayscale transition hover:hue-rotate-0 hover:sepia-0 dark:sepia"
             src={profilePic}
             alt="Luciano Ramello"
             width={150}
