f3ba943aa06ad7cf03bd799239b23b21128b5882
Luciano Ramello <luchosr@gmail.com>
2024-10-21 20:48:22 +0200

    feat: improves navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,26 @@
-import Header from './components/Header';
+import { useState } from "react";
+import Header from "./components/Header";
+import DarkModeButton from "./components/DarkModeButton";
 
 function App() {
+  const [dark, setDark] = useState(true);
+
+  const darkModeHandler = () => {
+    setDark(!dark);
+    document.body.classList.toggle("dark");
+  };
   return (
-    <main className="py-32 max-w-2xl mx-auto flex flex-col gap-32 px-4">
+    <main className="text-brown-900 flex h-screen flex-col items-center bg-orange-100 dark:bg-gray-800 dark:text-slate-200">
+      <nav className="flex w-3/4 flex-row justify-end border border-black pt-4 xl:w-1/2">
+        <DarkModeButton dark={dark} darkModeHandler={darkModeHandler} />
+      </nav>
       <Header />
+
+      <section>
+        <h2 className="text-center text-2xl no-underline md:text-left">
+          Here are some of my latest projects
+        </h2>
+      </section>
     </main>
   );
 }
