7598663a8b2319f19208927c622f1dff2aa6f74a
Luciano Ramello <luchosr@gmail.com>
2024-10-22 13:42:20 +0200

    feat: adds navbar component

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,16 @@
+import React, { useState } from "react";
+import DarkModeButton from "./DarkModeButton";
+
+export default function Navbar() {
+  const [dark, setDark] = useState(true);
+  const darkModeHandler = () => {
+    setDark(!dark);
+    document.body.classList.toggle("dark");
+    console.log("dark is: ", dark);
+  };
+  return (
+    <nav className="flex w-3/4 flex-row justify-end pt-4 xl:w-1/2">
+      <DarkModeButton dark={dark} darkModeHandler={darkModeHandler} />
+    </nav>
+  );
+}
