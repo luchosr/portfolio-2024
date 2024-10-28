377130a472e653ce0686284d437a5aa16947aeb8
Luciano Ramello <luchosr@gmail.com>
2024-10-28 17:01:08 +0100

    style: improves header styling

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,11 +3,11 @@ import profilePic from "../images/profilePic.webp";
 export default function Header() {
   return (
     <header className="mx-auto mt-12 flex max-w-2xl flex-col items-center md:flex-row">
-      <div className="mt-2 flex-1 md:mt-0 md:w-2/3">
-        <h1 className="text-center text-3xl no-underline md:text-left">
+      <div className="mt-2 flex-1 px-6 md:mt-0 md:w-2/3">
+        <h1 className="text-start text-3xl no-underline dark:text-orange-200 md:text-left md:text-center">
           Hey, I&#39;m Luciano.
         </h1>
-        <p className="mb-6 mt-3 text-center md:text-left">
+        <p className="mb-6 mt-3 text-start md:text-left md:text-center">
           I&#39;m a <span className="font-bold">React.js | Next.js</span>{" "}
           frontend developer based in Madrid, Spain. I&#39;m passionate about
           learning new technologies as <span className="font-bold">Rust</span>,
@@ -24,8 +24,8 @@ export default function Header() {
             className="flex-1 cursor-pointer rounded-lg bg-white/30 grayscale transition hover:hue-rotate-0 hover:sepia-0 dark:sepia"
             src={profilePic}
             alt="Luciano Ramello"
-            width={150}
-            height={175}
+            width={120}
+            height={150}
           />
         </a>
       </div>
