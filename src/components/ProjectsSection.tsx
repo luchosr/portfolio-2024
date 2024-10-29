35120fec57cd45e1f63554351bcb30232db6ade0
Luciano Ramello <luchosr@gmail.com>
2024-10-29 17:58:08 +0100

    feat: implements project card iteration

--- src/components/ProjectsSection.tsx
+++ src/components/ProjectsSection.tsx
@@ -1,4 +1,5 @@
-import { IoBeer } from "react-icons/io5";
+import { PROJECTS_DATA } from "../data/projects";
+import ProjectCard from "./ProjectCard";
 
 export default function ProjectsSection() {
   return (
@@ -6,30 +7,9 @@ export default function ProjectsSection() {
       <h2 className="text-start text-2xl underline dark:text-orange-200">
         Here are some of my latest projects:
       </h2>
-      <article className="my-4 flex w-full flex-row rounded-md border border-transparent px-2 hover:border-slate-300">
-        <div className="flex flex-col px-4 py-2">
-          <h3 className="text-xl font-bold">Drinks Cart App</h3>
-          <p className="text-sm">
-            <span>Stack: </span>React,js, Zustand, TailwindCSS
-          </p>
-        </div>
-        <a
-          href="https://github.com/luchosr/drinks-cart-app"
-          target="_blank"
-          rel=" noopener noreferrer"
-          className="m-auto w-1/5 text-2xl"
-          aria-label="Drinks Cart App"
-        >
-          <IoBeer />
-          {/* <img
-            src="https://raw.githubusercontent.com/luchosr/drinks-cart-app/refs/heads/main/public/drinks%20recipe%20main.png"
-            alt=""
-            width={80}
-            height={80}
-            loading="lazy"
-          /> */}
-        </a>
-      </article>
+      {PROJECTS_DATA.map((project) => (
+        <ProjectCard {...project} key={project.name} />
+      ))}
     </section>
   );
 }
