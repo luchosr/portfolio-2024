5a69e91f3dcd2e85b5381f675902990d3cc4c0c0
Luciano Ramello <luchosr@gmail.com>
2024-10-30 18:13:41 +0100

    fix: improved projects section

--- src/components/ProjectsSection.tsx
+++ src/components/ProjectsSection.tsx
@@ -3,7 +3,7 @@ import ProjectCard from "./ProjectCard";
 
 export default function ProjectsSection() {
   return (
-    <section className="mt-10 w-full max-w-2xl px-6">
+    <section className="mt-10 w-full max-w-2xl items-center px-6">
       <h2 className="text-start text-2xl underline dark:text-orange-200">
         Here are some of my recent projects:
       </h2>
