518ed054522a226fe7ea7ef8764e4af67d7298c3
Luciano Ramello <luchosr@gmail.com>
2024-10-30 09:54:37 +0100

    feat: adds string constants

--- src/data/stringConstants.ts
+++ src/data/stringConstants.ts
@@ -1,4 +1,7 @@
-export const description =
-  "I'm a frontend developer based in Madrid, Spain, passionate about learning new technologies as Rust and TypeScript and sharing knowledge with others.";
-
+export const DESCRIPTION =
+  "I'm a frontend developer based in Madrid, Spain, passionate about learning new technologies and sharing knowledge with others.";
 export const PROFILE_TITLE = "Hello, I'm Luciano.";
+export const GITHUB_PROFILE = "https://github.com/luchosr";
+export const LINKEDIN_PROFILE =
+  "https://www.linkedin.com/in/luciano-ramello-b45411143/";
+export const FOOTER_RIGHTS = "© Luciano Ramello. All rights reserved.";
