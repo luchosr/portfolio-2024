dd5c6e7d1e9b67ab57936a02a50a322c8a864667
Luciano Ramello <luchosr@gmail.com>
2024-10-30 09:55:36 +0100

    fix: updates string constants

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,15 +1,16 @@
 import profilePic from "../images/profilePiccrop.webp";
-import { description, PROFILE_TITLE } from "../data/stringConstants";
+import {
+  DESCRIPTION,
+  LINKEDIN_PROFILE,
+  PROFILE_TITLE,
+} from "../data/stringConstants";
 export default function Header() {
   return (
     <header className="mx-auto mt-12 flex max-w-2xl flex-col items-center md:flex-row md:px-6">
       <div className="relative p-2">
-        <a
-          href="https://www.linkedin.com/in/luciano-ramello-b45411143/"
-          target="_blank"
-        >
+        <a href={LINKEDIN_PROFILE} target="_blank">
           <img
-            className="flex-1 cursor-pointer rounded-lg bg-white/30 grayscale transition hover:hue-rotate-0 hover:sepia-0 dark:sepia"
+            className="flex-1 cursor-pointer rounded-lg grayscale transition hover:hue-rotate-0 hover:sepia-0 dark:sepia"
             src={profilePic}
             alt="Luciano Ramello"
             width={120}
@@ -22,7 +23,7 @@ export default function Header() {
         <h1 className="text-start text-3xl no-underline dark:text-orange-200">
           {PROFILE_TITLE}
         </h1>
-        <p className="mb-6 mt-3 text-start">{description}</p>
+        <p className="mb-6 mt-3 text-start">{DESCRIPTION}</p>
       </div>
     </header>
   );
