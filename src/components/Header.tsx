f5579a366776b0df2b3b66eb652950f8b3583d05
Luciano Ramello <luchosr@gmail.com>
2024-10-30 18:20:09 +0100

    build: updates new build

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -9,14 +9,16 @@ export default function Header() {
     <header className="mx-auto mt-12 flex max-w-2xl flex-col items-center md:flex-row md:px-6">
       <div className="relative p-2">
         <a href={LINKEDIN_PROFILE} target="_blank">
-          <img
-            className="flex-1 cursor-pointer rounded-lg grayscale transition hover:hue-rotate-0 hover:sepia-0 dark:sepia"
-            src={profilePic}
-            alt="Luciano Ramello"
-            width={120}
-            height={150}
-            loading="lazy"
-          />
+          <figure>
+            <img
+              className="flex-1 cursor-pointer rounded-lg grayscale transition hover:hue-rotate-0 hover:sepia-0 dark:sepia"
+              src={profilePic}
+              alt="Luciano Ramello"
+              width={120}
+              height={150}
+              loading="lazy"
+            />
+          </figure>
         </a>
       </div>
       <div className="mt-2 flex-1 px-6 md:mt-0 md:w-2/3">
