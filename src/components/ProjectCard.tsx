f23d86200d4098b67740ee57120157928c8839b5
Luciano Ramello <luchosr@gmail.com>
2024-10-30 12:26:04 +0100

    fix: updates links for projects

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -15,27 +15,27 @@ export default function ProjectCard({
 }: ProjectCardProps) {
   return (
     <article className="my-4 flex max-w-xl flex-row rounded-md border border-slate-800 px-2 dark:border-slate-300">
-      <div className="flex flex-col px-4 py-2">
-        <div className="space-around flex w-2/3 flex-row py-2 md:w-2/5">
-          <h3 className="text-xl font-bold">{name}</h3>
-          <a
-            href={link}
-            target="_blank"
-            rel=" noopener noreferrer"
-            className="m-auto w-1/5 text-2xl"
-            aria-label="Drinks Cart App"
-          >
-            <IoLogoGithub />
-          </a>
-        </div>
+      <a
+        href={link}
+        target="_blank"
+        rel=" noopener noreferrer"
+        aria-label="Drinks Cart App"
+      >
+        <div className="flex flex-col px-4 py-2">
+          <div className="space-around flex w-2/3 flex-row py-2 md:w-2/5">
+            <h3 className="text-xl font-bold">{name}</h3>
 
-        <p className="text-sm">{description}</p>
-        <div className="my-4 flex flex-row flex-wrap">
-          {stack.map((tech) => (
-            <TechChip key={tech} tech={tech} />
-          ))}
+            <IoLogoGithub className="m-auto w-1/5 text-2xl" />
+          </div>
+
+          <p className="text-sm">{description}</p>
+          <div className="my-4 flex flex-row flex-wrap">
+            {stack.map((tech) => (
+              <TechChip key={tech} tech={tech} />
+            ))}
+          </div>
         </div>
-      </div>
+      </a>
     </article>
   );
 }
