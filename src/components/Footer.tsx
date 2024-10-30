f4889266f5986b35034c608805b2fde0530cc89e
Luciano Ramello <luchosr@gmail.com>
2024-10-30 09:55:14 +0100

    feat: adds footer component

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,24 @@
+import { IoLogoGithub, IoLogoLinkedin } from "react-icons/io5";
+import {
+  FOOTER_RIGHTS,
+  GITHUB_PROFILE,
+  LINKEDIN_PROFILE,
+} from "../data/stringConstants";
+
 export default function Footer() {
   return (
-    <footer className="flex w-3/4 flex-row justify-end pt-4 md:w-1/3">
-      Footer
+    <footer className="m-auto mb-6 flex max-w-2xl flex-row justify-start px-6 pt-4 dark:text-orange-skin">
+      <article className="text-sm font-semibold">
+        {new Date().getFullYear()} {FOOTER_RIGHTS}
+      </article>
+      <div className="mx-6 flex flex-row">
+        <a href={LINKEDIN_PROFILE} target="_blank">
+          <IoLogoLinkedin className="mx-2 text-xl" />
+        </a>
+        <a href={GITHUB_PROFILE} target="_blank">
+          <IoLogoGithub className="mx-2 text-xl" />
+        </a>
+      </div>
     </footer>
   );
 }
