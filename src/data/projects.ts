4d7bb7dca2e28fbff2454417657e7c8d87558a96
Luciano Ramello <luchosr@gmail.com>
2024-10-30 18:14:32 +0100

    fix: improved projects info with assets

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -1,17 +1,23 @@
+import veterinaryPic from "../images/Veterinary-patients.webp";
+import drinkCartPic from "../images/Drinks-cart.webp";
+import caloryTrackerPic from "../images/Calory-tracker.webp";
+
 export const PROJECTS_DATA = [
   {
     name: "Drinks Cart App",
     description:
-      "In this app you can put an ingredient or a name of a drink on the input and search on an API database all the recipes related to it.",
+      "In this app you can put an ingredient or a name of a drink on the input and search on an API database all the drinks recipes related to it, and if you like the drink you can store it on your favorites section.",
     link: "https://github.com/luchosr/drinks-cart-app",
     stack: ["React", "TypeScript", "Zustand", "ZOD", "TailwindCSS"],
+    imgsrc: drinkCartPic,
   },
   {
     name: "Veterinary Patients App",
     description:
       "This is an app to keep track of veterinary patients, you can create a ticket as an appointment with  patient's status, edit it and delete it.",
-    link: "https://github.com/luchosr/calories-counter",
+    link: "https://github.com/luchosr/veterinary-patients",
     stack: ["React", "TypeScript", "Zustand", "TailwindCSS"],
+    imgsrc: veterinaryPic,
   },
   {
     name: "Calories Tracker",
@@ -19,5 +25,6 @@ export const PROJECTS_DATA = [
       "This is an app to track daily calories burned during exercise or added up in food intake.",
     link: "https://github.com/luchosr/calories-counter",
     stack: ["React", "TypeScript", "Context API", "TailwindCSS"],
+    imgsrc: caloryTrackerPic,
   },
 ];
