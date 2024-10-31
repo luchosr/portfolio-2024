524eedd6170a981959b0e90bc2e91b1db3423da2
Luciano Ramello <luchosr@gmail.com>
2024-10-31 10:00:26 +0100

    fix: improves projects url

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -7,7 +7,7 @@ export const PROJECTS_DATA = [
     name: "Drinks Cart App",
     description:
       "In this app you can put an ingredient or a name of a drink on the input and search on an API database all the drinks recipes related to it, and if you like the drink you can store it on your favorites section.",
-    link: "https://github.com/luchosr/drinks-cart-app",
+    link: "https://friendly-manatee-45e935.netlify.app/",
     stack: ["React", "TypeScript", "Zustand", "ZOD", "TailwindCSS"],
     imgsrc: drinkCartPic,
   },
@@ -15,7 +15,7 @@ export const PROJECTS_DATA = [
     name: "Veterinary Patients App",
     description:
       "This is an app to keep track of veterinary patients, you can create a ticket as an appointment with  patient's status, edit it and delete it.",
-    link: "https://github.com/luchosr/veterinary-patients",
+    link: "https://veterinary-patients-ten.vercel.app",
     stack: ["React", "TypeScript", "Zustand", "TailwindCSS"],
     imgsrc: veterinaryPic,
   },
@@ -23,7 +23,7 @@ export const PROJECTS_DATA = [
     name: "Calories Tracker",
     description:
       "This is an app to track daily calories burned during exercise or added up in food intake.",
-    link: "https://github.com/luchosr/calories-counter",
+    link: "https://calories-counter-rho.vercel.app/",
     stack: ["React", "TypeScript", "Context API", "TailwindCSS"],
     imgsrc: caloryTrackerPic,
   },
